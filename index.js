59149d0669fc28ba99beaa9436be380b432347ad
Robin Altman <robinhaltman@gmail.com>
2023-11-20 17:30:37 -0500

    pokemon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { sentence, paragraph } from 'txtgen'
+import fetch from 'node-fetch'
 
 // Create an instance of an express application 
 const app = express()
@@ -9,10 +10,16 @@ const port = process.env.PORT || 3001
 
 // Set up a response for the root path of the application
 app.get('/', (req, res) => {
-  res.send(paragraph([20]))
+  fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+  .then(response => response.json())
+  .then(bulbasaur =>{
+  res.send(bulbasaur.moves)
+})
 })
 
 // Set the application to listen a port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}` )
 })
+
+
